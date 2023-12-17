1c657c44866f6338bdf5008e23d672d3ef796125
yobs <devnoyo0123@gmail.com>
2023-12-18 00:15:30 +0900

    producer 모듈 추가

---
+++ module-common/src/main/java/com/example/modulecommon/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,9 @@
+package com.example.modulecommon.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
+
