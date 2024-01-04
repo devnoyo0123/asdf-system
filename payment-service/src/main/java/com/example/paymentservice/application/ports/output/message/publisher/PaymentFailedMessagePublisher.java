d73a8191c29605efb46ffaba65a3721cef97837d
yobs <devnoyo0123@gmail.com>
2024-01-05 01:47:17 +0900

    payment-service 메세지 퍼블리셔 구현추가

--- payment-service/src/main/java/com/example/paymentservice/application/ports/output/message/publisher/PaymentFailedMessagePublisher.java
+++ payment-service/src/main/java/com/example/paymentservice/application/ports/output/message/publisher/PaymentFailedMessagePublisher.java
@@ -1,7 +1,7 @@
 package com.example.paymentservice.application.ports.output.message.publisher;
 
 import com.example.modulecommon.domain.event.publisher.DomainEventPublisher;
-import com.example.paymentservice.domain.event.PaymentCancelledEvent;
+import com.example.paymentservice.domain.event.PaymentFailedEvent;
 
-public interface PaymentFailedMessagePublisher extends DomainEventPublisher<PaymentCancelledEvent> {
+public interface PaymentFailedMessagePublisher extends DomainEventPublisher<PaymentFailedEvent> {
 }
