151c70b4cc2a54df8b86f5721aec5886571dbb14
yobs <devnoyo0123@gmail.com>
2024-01-05 00:44:37 +0900

    customer-service 추가

--- order-service/src/main/java/com/example/orderservice/adapter/out/repository/customer/CustomerRepositoryImpl.java
+++ order-service/src/main/java/com/example/orderservice/adapter/out/repository/customer/CustomerRepositoryImpl.java
@@ -23,6 +23,6 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public Optional<Customer> findCustomer(UUID customerId) {
-        return Optional.empty();
+        return customerJpaRepository.findById(customerId).map(customerDataAccessMapper::customerEntityToCustomer);
     }
 }
